d936db75e47507ae5b67b47580b3e03101e8f5d2
shane knapp <incomplete@gmail.com>
2025-05-13 16:51:10 -0700

    stop the infinite deathloop of redirects

--- extra-assets/js/login.js
+++ extra-assets/js/login.js
@@ -1,11 +1,11 @@
-function redirectIfNeeded() {
-    // Only redirect utoronto.2i2c.cloud, lets us keep staging at staging.utoronto.2i2c.cloud
-    if (window.location.hostname === 'jupyter.cal-icor.org') {
-        // Let's give users an indication that something is happening
-        document.write("Redirecting you to jupyter.cal-icor.org...");
-        window.location.hostname = 'jupyter.cal-icor.org';
-    }
-}
+// function redirectIfNeeded() {
+//     // Only redirect utoronto.2i2c.cloud, lets us keep staging at staging.utoronto.2i2c.cloud
+//     if (window.location.hostname === 'some.host.name') {
+//         // Let's give users an indication that something is happening
+//         document.write("Redirecting you to jupyter.cal-icor.org...");
+//         window.location.hostname = 'jupyter.cal-icor.org';
+//     }
+// }
 function setInterface(interfaceUrl) {
     let loginUrl = new URL($('#home').data('authenticator-login-url'), document.location.origin);
     loginUrl.searchParams.set('next', '/hub/user-redirect/' + interfaceUrl)
